aaa0869d55a392786e4e78be271cf35e1a1be07b
Justin Neeb <neebjustin@gmail.com>
2018-03-19 23:01:33 -0400

    added picture album functionailty and all event image libraries, added videos page with embeded youtube videos

---
+++ site/functions/slick.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $('.slider-for').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        adaptiveHeight: true,
+        fade: true,
+        asNavFor: '.slider-nav'
+    });
+    $('.slider-nav').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        asNavFor: '.slider-for',
+        centerMode: true,
+        focusOnSelect: true
+    });
+});
