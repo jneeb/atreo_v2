8e8263973aab7efd1c1e7ac1dd988a058a712b3d
Justin Neeb <neebjustin@gmail.com>
2019-02-07 02:24:54 -0500

    updated copywriting and forms for guest list and bottle service

--- site/king-cruise/contactFormBS.php
+++ site/king-cruise/contactFormBS.php
@@ -1,17 +1,19 @@
 <?php
     if(isset($_POST['submit'])) {
-        $myemail = 'atreoentertainment@gmail.com';
+        $myemail = 'neebjustin@gmail.com';
         $name = $_POST['fullName'];
         $bottleAmount = $_POST['bottleAmount'];
         $phone = $_POST['number'];
         $email = $_POST['email'];
+        $boothSize = $_POST['boothSize'];
         $comments = $_POST['notes'];
         $subject = "Atreo Bottle Service";
         $message = "Name: $name
-        Bottle Amount: $bottleAmount
-        Phone: $phone
-        Email: $email
-        Notes: $comments";
+Booth Size: $boothSize
+Bottle Package: $bottleAmount
+Phone: $phone
+Email: $email
+Notes: $comments";
         mail($myemail, $subject, $message);
         header('Location: formComplete.html');
         exit();
--- site/king-cruise/contactFormGL.php
+++ site/king-cruise/contactFormGL.php
@@ -1,6 +1,6 @@
 <?php
     if(isset($_POST['submit'])) {
-        $myemail = 'atreoentertainment@gmail.com';
+        $myemail = 'neebjustin@gmail.com';
         $name = $_POST['fullName'];
         $guests = $_POST['guestAmount'];
         $phone = $_POST['number'];
@@ -8,10 +8,10 @@
         $comments = $_POST['notes'];
         $subject = "Atreo Guest List";
         $message = "Name: $name
-        Guest Amount: $guests
-        Phone: $phone
-        Email: $email
-        Notes: $comments";
+Guest Amount: $guests
+Phone: $phone
+Email: $email
+Notes: $comments";
         mail($myemail, $subject, $message);
         header('Location: formComplete.html');
         exit();
